df92d1c7be02def350aa4ac60c0953c3b47d1b5a
Rajendra <rajendradanve@gmail.com>
2021-09-10 14:11:54 +0000

    started commenting and star rating

--- static/js/script.js
+++ static/js/script.js
@@ -2,5 +2,14 @@ $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.modal').modal();
     $('select').formSelect();
-  
+
+    let star_rating = $("input[name='star_rating'")
+    for(let i=0; i<star_rating.length; i++){
+      star_rating[i].click(function(){
+        i = this.val();
+        console.log("test");
+        $("#rating_value").text("your are best");
+      })
+    }
+
   });
